7c58132836acbeaae3df7e0bd12baab9bb02b160
Eltorretaz <eltorretaz470@gmail.com>
2023-11-13 20:39:54 -0400

    reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let string = "";
+    let count = word.length;
+    for(let i = 0; i < word.length; i++){
+        count--;
+        string += word[count];
+    }
+    return (string);
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...anyMoreItems) {
 
+    let string = "";
+
+    for(let i = 0; i < anyMoreItems.length; i++){
+        arr.in
+        string = arr.splice;
+    }
+    return arr;
 };
 
 // Do not edit below this line
