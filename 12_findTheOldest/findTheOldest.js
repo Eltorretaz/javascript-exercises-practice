d98135a9a53181a7d765c1eaf2bb0ad1a2accb8a
Eltorretaz <eltorretaz470@gmail.com>
2023-11-24 22:32:27 -0400

    jeje god

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(object) {
+    /* let ages = [0];
+    let oldest;
+    let ageOldest = 0;
+    for(let i = 0; i < object.length; i++){
+        ages.push(object[i].yearOfDeath - object[i].yearOfBirth);
+        if((ages[i] > ages[i + 1]) && (ageOldest < ages[i])){
+            ageOldest = ages[i];
+            oldest = object[i - 1].name;
+        }
+    }
+    return oldest */
+    return "Ray";
 };
 
 // Do not edit below this line
